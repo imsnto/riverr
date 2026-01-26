d397f5989bb93495e4ab55a1316abb14f7177323
Bradley Miller <brad@riverr.app>
2026-01-26 09:02:38 +0000

    make this icon a chat icon instead of the star

--- src/components/dashboard/bot-settings-dialog.tsx
+++ src/components/dashboard/bot-settings-dialog.tsx
@@ -480,9 +480,7 @@ export default function BotSettingsDialog({
                     style={{ backgroundColor: watchedValues.primaryColor }}
                     onClick={() => setIsPreviewMinimized(false)}
                 >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="shrink-0 text-white">
-                        <path d="M12 2L14.5 9.5L22 12L14.5 14.5L12 22L9.5 14.5L2 12L9.5 9.5L12 2Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
+                    <MessageSquare className="h-7 w-7 text-white" />
                 </div>
             ) : (
              <>
@@ -586,5 +584,3 @@ export default function BotSettingsDialog({
     </Dialog>
   );
 }
-
-    
